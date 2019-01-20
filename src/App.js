55bc021e549323bfed2e20ab903f05eee1762ca3
Puyan Wei <puyanwei@gmail.com>
2019-01-20 21:42:28 +0000

    Updating readme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { value: "" };
+  }
+
+  handleTextboxValue = event => {
+    this.setState({ value: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <div className="element">
-          Tell me your darkest secrets! <input type="text" />
+          Tell me your darkest secrets!
+          <br />
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleTextboxValue}
+          />
         </div>
       </div>
     );
