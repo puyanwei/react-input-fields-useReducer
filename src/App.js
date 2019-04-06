af5422cda0417fe919c1852ffe21a64cd557020a
Puyan Wei <puyanwei@gmail.com>
2019-04-06 13:10:54 +0100

    Refactored to use useReducer instead of useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
 const App = () => {
-	const [ secret, setSecret ] = useState('');
-
-	const handleTextboxValue = (e) => {
-		setSecret(e.target.value);
-	};
+	const initialState = '';
+	const [ secret, setSecret ] = useReducer(reducer, initialState);
 
 	return (
 		<div className="App">
 			<div className="element">
 				Tell me your darkest secret!
 				<br />
-				<input type="text" value={secret} onChange={(e) => handleTextboxValue(e)} />
+				<input type="text" value={secret} onChange={(e) => setSecret(e.target.value)} />
 				<br />
-				<h2>Shhh, don't tell anyone but {secret}</h2>
+				<h2>
+					Shhh, don't tell anyone but...<br />
+					"{secret}"
+				</h2>
 			</div>
 		</div>
 	);
+};
+
+const reducer = (state, action) => {
+	return action;
 };
 
 export default App;
